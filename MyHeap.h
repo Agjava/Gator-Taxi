c08485c0677752d292b1f18707ee9ea8e680b497
Anvesh gupta <102137097+Anvesh2522@users.noreply.github.com>
2023-04-11 19:22:37 -0400

    Update MyHeap.h

--- MyHeap.h
+++ MyHeap.h
@@ -76,7 +76,7 @@ else if(rcp > rcc) {           // checks if the rideCost of the parent node is g
 swap(heap[parent] -> index, heap[childIndex] -> index);
 swap(heap[parent], heap[childIndex]);
  }
-else if(ptd > ctd) {          //checks if the tripDuration of the parent node is greater than that of the child node. If it is, swap the values of the index and heap members for the parent and child nodes.
+else if(tdp > tdc) {          //checks if the tripDuration of the parent node is greater than that of the child node. If it is, swap the values of the index and heap members for the parent and child nodes.
 swap(heap[parent] -> index, heap[childIndex] -> index);
 swap(heap[parent], heap[childIndex]);
 } 
@@ -98,45 +98,44 @@ if(rcp > rclc || tdp > tdlc )    //checks if either the rideCost or tripDuration
 swapchild = leftChild;
  } 
  else {
-int rcrc = heap[rightChild] -> rideCost, 
-int tdrc = heap[rightChild] -> tripDuration;       //initalizing variables
-if(prc < lcrc && prc < rcrc) return;          // checks if both children have a smaller ride cost and trip duration than their parent. If so, it means that no swap is needed and returns immediately.
-if(prc > lcrc || ptd > lctd) swapchild = leftChild;    //checks if either ride cost or trip duration of parent is greater than that of left child. If so, it sets childToSwap to be left child.
-else if(prc > rcrc || ptd > rctd) swapchild = rightChild;    //checks if either ride cost or trip duration of parent is greater than that of right child. If so, it sets childToSwap to be right child.
+int rcrc = heap[rightChild] -> rideCost, tdrc = heap[rightChild] -> tripDuration;       //initalizing variables
+if(rcp < rclc && rcp < rcrc) return;          // checks if both children have a smaller ride cost and trip duration than their parent. If so, it means that no swap is needed and returns immediately.
+if(rcp > rclc || tdp > tdlc) swapchild = leftChild;    //checks if either ride cost or trip duration of parent is greater than that of left child. If so, it sets childToSwap to be left child.
+else if(rcp > rcrc || tdp > tdrc) swapchild = rightChild;    //checks if either ride cost or trip duration of parent is greater than that of right child. If so, it sets childToSwap to be right child.
  }
 swap(heap[parent] -> index, heap[swapchild] -> index);     //swapping
 swap(heap[parent], heap[swapchild]);
 heapifyDown(swapchild);
 }
 
-void update(HeapNode* heapNode, int newRideCost, int newTripDuration) {
-if(newRideCost > heapNode -> rideCost) {
-heapNode -> rideCost = newRideCost;
+void update(HeapNode* heapNode, int newRideCost, int newTripDuration) {    //defines the update function that takes a pointer to a HeapNode object, an integer newRideCost, and an integer newTripDuration as arguments and returns nothing (void).
+if(newRideCost > heapNode -> rideCost) {              //checks if the new ride cost is greater than the current ride cost of the heap node.
+heapNode -> rideCost = newRideCost;                
 heapifyDown(heapNode -> index);
 } 
-else if(newTripDuration <= heapNode -> tripDuration) {
+else if(newTripDuration <= heapNode -> tripDuration) {  //checks if the new trip duration is less than or equal to the current trip duration of the heap node.
 heapNode -> tripDuration = newTripDuration;
 heapifyUp(heapNode -> index);
 } 
-else if(newTripDuration > heapNode -> tripDuration) {
+else if(newTripDuration > heapNode -> tripDuration) {   //checks if the new trip duration is greater than the current trip duration of the heap node.
 heapNode -> tripDuration = newTripDuration;
 heapifyDown(heapNode -> index);
 }
 }
-void remove(HeapNode* heapNode) {
+void remove(HeapNode* heapNode) {    //defines a function named remove that takes a pointer to a HeapNode object as an argument and returns nothing (void)
 int index = heapNode -> index;
 back = back -1;
 heap[index] = heap[back];
 heap[index] -> index = index;
 heapifyDown(index);
 }
-void print() {
+void print() {            //defines a function named print that takes no arguments and returns nothing (void).
 int level = 0, limit = 0;
 for(int i = 0; i < back; i++) {
 heap[i] -> HeapNodeprint();
 cout << ", ";
-if(i == limit) {
-limit = limit + pow(2, ++level);
+if(i == limit) {     //checks if i is equal to limit which represents end of current level in binary tree representation of binary heap data structure. 
+limit = limit + pow(2, ++level);      //this line increments level by 1 and assigns value of limit plus power of 2 raised to level+1 to limit which represents end of next level in binary tree representation of binary heap data structure.
 cout << endl;
 }
 }
